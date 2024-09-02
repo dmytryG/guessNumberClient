2fd8f16b68a9db476ae0930d4f9f321ccfd96ece
Голуб Дмитрий <dmytrygolub@gmail.com>
2024-09-02 15:52:39 +0300

    debug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {HashRouter, Route, Routes} from "react-router-dom";
+// import {HashRouter, Route, Routes} from "react-router-dom";
 // import {Route, Routes, HashRouter} from 'react-router-dom';
 // import Play from "./pages/Play";
 const {SetAPI} = require('./pages/SetAPI')
@@ -9,12 +9,12 @@ export default function App() {
   return (
     <div className="App">
       before router
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<SetAPI />}/>
-        <Route path="/play" element={<Play />}/>
-      </Routes>
-    </HashRouter>
+    {/*<HashRouter>*/}
+    {/*  <Routes>*/}
+    {/*    <Route path="/" element={<SetAPI />}/>*/}
+    {/*    <Route path="/play" element={<Play />}/>*/}
+    {/*  </Routes>*/}
+    {/*</HashRouter>*/}
       after router
       <SetAPI />
     </div>
