058c1b1716f821f320f133f4032420a0e65f9182
Голуб Дмитрий <dmytrygolub@gmail.com>
2024-09-02 14:41:22 +0300

    test of working as TWA

--- src/config/numberState.ts
+++
@@ -1,5 +0,0 @@
-export const numberState = {
-  HIGHER: 'HIGHER',
-  LOWER: 'LOWER',
-  GUESSED: 'GUESSED'
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  },
+  plugins: [react(), basicSsl()],
   build: {
     outDir: './dist'
-  }
+  },
+  base: '/vite-boilerplate/'
 });
