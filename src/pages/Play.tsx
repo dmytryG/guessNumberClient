054b1eba4d586dee0553d108929d4c8731b9d5ae
Голуб Дмитрий <dmytrygolub@gmail.com>
2024-09-02 13:18:46 +0300

    adding router

---
+++ src/pages/Play.tsx
@@ -0,0 +1,14 @@
+export function Play() {
+    return (
+        <div className={"modal-container modal-container-disable-background"}>
+            <div className={"modal-child"}>
+                <div className={"modal-header"}>
+                    <h1>Guess the number</h1>
+                </div>
+                <div className={"modal-content"}>
+                    <button className={"full-width"}>Start</button>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/SetAPI.tsx
@@ -0,0 +1,16 @@
+export function SetAPI() {
+    return (
+            <div className={"modal-container modal-container-disable-background"}>
+                <div className={"modal-child"}>
+                    <div className={"modal-header"}>
+                        <h1>Guess the number</h1>
+                    </div>
+                    <div className={"modal-content"}>
+                        <p>Please, input API URL in order to continue</p>
+                        <input/>
+                        <button className={"full-width"}>Ok</button>
+                    </div>
+                </div>
+            </div>
+    );
+}
