f1075141772d54808ec27d571e45bbd2d80ea896
Голуб Дмитрий <dmytrygolub@gmail.com>
2024-09-02 20:16:07 +0300

    Fixed telegram ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+const {useEffect} = require("react");
 // import {HashRouter, Route, Routes} from "react-router-dom";
 const {Route, Routes, BrowserRouter} = require('react-router-dom');
 const {SetAPI} = require('./pages/SetAPI')
 const {Play} = require('./pages/Play')
 
 export default function App() {
+    useEffect(() => {
+        window.Telegram.WebApp.ready()
+    }, []);
+
   return (
     <div className="App">
         <div>
